6c554e49932f2b83efa7deab641dd44cc449ced6
victoria633 <victoria6411@hotmail.com>
2018-10-31 23:49:08 +0000

    automatically backed up by learn

--- silly_string2.rb
+++ silly_string2.rb
@@ -10,4 +10,5 @@ puts "My favorite youtuber is #{favorite_youtuber_all_caps}".upcase
 
 # Now let's make all the characters lowercase in the string
 phrase = "I'M NOT SHOUTING".downcase
-
+ 
+ lower
