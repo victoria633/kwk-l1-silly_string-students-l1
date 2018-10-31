3e0a26faf1405d8e9719ccc9a40ff029836d9c1b
victoria633 <victoria6411@hotmail.com>
2018-10-31 23:40:07 +0000

    automatically backed up by learn

--- silly_string2.rb
+++ silly_string2.rb
@@ -1,11 +1,11 @@
-#Create a string that contains your favorite youtuber.
+# Create a string that contains your favorite youtuber.
 
 favorite_youtuber = "Joey Kidney"
 puts "My favorite youtuber is #{favorite_youtuber}"
 
-#Using a method built into ruby, capitalize every letter in the string you just created
+# Using a method built into ruby, capitalize every letter in the string you just created
 
 favorite_youtuber_all_caps = favorite_youtuber
 puts "My favorite youtuber is #{favorite_youtuber_all_caps}".upcase
 
-# 
+# Now let's make all the characters lowercase in the string
